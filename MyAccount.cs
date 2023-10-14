24cd61d053e44d1f25a2bb11532e5ac26671db5b
Ивана Новаковиќ <ivche@Ivanas-MacBook-Air.local>
2023-10-14 16:52:12 +0200

    first commit

---
+++ MyAccount.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace FinalAutomation
+{
+    public class MyAccount
+    {
+        public IWebDriver driver;
+
+        public MyAccount(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void LoginAccount(string usernameOrEmail, string password)
+        {
+            IWebElement myAccountLink = driver.FindElement(By.CssSelector("#menu-item-5294 > a"));
+            myAccountLink.Click();
+
+            IWebElement accountUsernameInput = driver.FindElement(By.Id("username"));
+            accountUsernameInput.SendKeys(usernameOrEmail);
+
+            IWebElement accountPasswordInput = driver.FindElement(By.Id("password"));
+            accountPasswordInput.SendKeys(password);
+
+            IWebElement loginButton = driver.FindElement(By.CssSelector("button[name=\"login\"]"));
+            loginButton.Click();
+        }
+
+        public void ChangeDetails(string firstName, string lastName, string displayName)
+        {
+
+            IWebElement firstNameInput = driver.FindElement(By.Id("account_first_name"));
+            firstNameInput.Clear();
+            firstNameInput.SendKeys(firstName);
+
+            IWebElement lastNameInput = driver.FindElement(By.Id("account_last_name"));
+            lastNameInput.Clear();
+            lastNameInput.SendKeys(lastName);
+
+            IWebElement displayNameInput = driver.FindElement(By.Id("account_display_name"));
+            displayNameInput.Clear();
+            displayNameInput.SendKeys(displayName);
+
+        }
+
+        public void ChangePassword(string currentPassword, string newPassword, string confirmNewPassword)
+        {
+            IWebElement currentPasswordInput = driver.FindElement(By.Id("password_current"));
+            currentPasswordInput.SendKeys(currentPassword);
+
+            IWebElement newPasswordInput = driver.FindElement(By.Id("password_1"));
+            newPasswordInput.SendKeys(newPassword);
+
+            IWebElement confirmNewPasswordInput = driver.FindElement(By.Id("password_2"));
+            confirmNewPasswordInput.SendKeys(confirmNewPassword);
+
+            
+        }
+
+        public void SaveChanges()
+        {
+            IWebElement saveChangesButton = driver.FindElement(By.CssSelector("button[name=\"save_account_details\"]"));
+            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+            jsExecutor.ExecuteScript("arguments[0].scrollIntoView()", saveChangesButton);
+
+            Thread.Sleep(2000);   //Find better solution for the thread sleep
+            saveChangesButton.Click();
+        }
+
+        public void Logout()
+        {
+            IWebElement logoutLink = driver.FindElement(By.CssSelector("li.woocommerce-MyAccount-navigation-link--customer-logout > a"));
+            logoutLink.Click();
+        }
+
+        public string GenerateRandomPassword(int length)
+        {
+            const string passwordCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!.,?{}[][";
+            Random random = new Random();
+
+            string randomPassword = new string(Enumerable.Repeat(passwordCharacters, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+
+            return randomPassword;
+        }
+
+    }
+}
+
+
---
+++ UnitTest1.cs
@@ -0,0 +1,110 @@
+﻿namespace FinalAutomation;
+
+using System;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+public class Tests
+{
+    public IWebDriver driver;
+    public MyAccount myAccount;
+
+
+    [SetUp]
+    public void Setup()
+    { 
+
+        driver = new ChromeDriver();
+
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+        driver.Manage().Window.Maximize();
+
+
+        driver.Navigate().GoToUrl("https://electro.madrasthemes.com/");
+
+        myAccount = new MyAccount(driver);
+
+    }
+
+
+    [Test]
+    public void Login()
+    {
+        myAccount.LoginAccount("ivanaautomation", "brainster123!");
+
+        IWebElement myAccountTitle = driver.FindElement(By.CssSelector("h1.entry-title"));
+        Assert.That(myAccountTitle.Text, Is.EqualTo("My Account"));
+
+        IWebElement username = driver.FindElement(By.CssSelector("div.woocommerce-MyAccount-content > p > strong"));
+        Assert.That(username.Text, Is.EqualTo("ivanaautomation"));
+    }
+
+    [Test]
+    public void ChangeAccountDetails()
+    {
+        myAccount.LoginAccount("ivanaautomation4", "brainster123!");
+
+        IWebElement accountDetailsLink = driver.FindElement(By.CssSelector("li.woocommerce-MyAccount-navigation-link--edit-account > a"));
+        accountDetailsLink.Click();
+
+        myAccount.ChangeDetails("Ivana1!", "Novakovikj", "ivanaautomationChangeDetails");
+        myAccount.SaveChanges();
+
+        IWebElement successMessage = driver.FindElement(By.CssSelector("div.woocommerce-message[role=\"alert\"]"));
+        Assert.That(successMessage.Text, Is.EqualTo("Account details changed successfully."));
+
+        IWebElement myAccountTitle = driver.FindElement(By.CssSelector("h1.entry-title"));
+        Assert.That(myAccountTitle.Text, Is.EqualTo("My Account"));
+
+    }
+
+    
+    [Test]
+    public void ChangePassword()
+    {
+
+        string currentPassword = File.ReadAllText("../../../password.txt");
+        string newPassword = myAccount.GenerateRandomPassword(10);
+        string confirmNewPassword = newPassword;
+
+        myAccount.LoginAccount("ivanaautomation3", currentPassword);
+
+        IWebElement accountDetailsLink = driver.FindElement(By.CssSelector("li.woocommerce-MyAccount-navigation-link--edit-account > a"));
+        accountDetailsLink.Click();
+
+        myAccount.ChangeDetails("Ivana1!", "Novakovikj", "ivanaautomationChangeDetails");
+
+        myAccount.ChangePassword(currentPassword, newPassword, confirmNewPassword);
+
+        myAccount.SaveChanges();
+
+        File.WriteAllText("../../../password.txt", newPassword);
+
+        //IWebElement successMessage = driver.FindElement(By.CssSelector("div.woocommerce-message[role=\"alert\"]"));
+        //Assert.That(successMessage.Text, Is.EqualTo("Account details changed successfully."));
+
+        //IWebElement myAccountTitle = driver.FindElement(By.CssSelector("h1.entry-title"));
+        //Assert.That(myAccountTitle.Text, Is.EqualTo("My Account"));
+
+        myAccount.Logout();
+
+        myAccount.LoginAccount("ivanaautomation3", newPassword);
+
+        IWebElement displayName = driver.FindElement(By.CssSelector("div.woocommerce-MyAccount-content > p > strong"));
+        Assert.That(displayName.Text, Is.EqualTo("ivanaautomationChangeDetails"));
+
+
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver.Quit();
+    }
+
+}
